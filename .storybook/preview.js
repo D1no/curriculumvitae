3424c357944083a196a2e7b75fe48260cc42b281
Dino Scheidt <github@dino-scheidt.de>
2022-03-07 21:15:56 +0100

    🔧 Adjusted storybook display and added viewports.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,32 @@
+import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
+import { themes } from "@storybook/theming";
+
+import {
+  goldenRatioLongContent,
+  goldenRatioShort,
+} from "@cv/views/DesignSystem";
 import DesignSystem from "@cv/views/DesignSystem";
 
 // TODO: Add ViewPort as Page Segments
 //       https://storybook.js.org/docs/react/essentials/viewport#add-new-devices
+
+const pageSegmentView = {
+  pageNarrow: {
+    name: "CV Section",
+    styles: {
+      width: `${goldenRatioShort}px`,
+      height: `${goldenRatioShort * 1.5}px`,
+    },
+  },
+  pageWide: {
+    name: "CV Content",
+    styles: {
+      width: `${goldenRatioLongContent}px`,
+      height: `${goldenRatioShort * 1.5}px`,
+    },
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -10,14 +35,25 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  layout: "fullscreen",
+  viewport: {
+    viewports: { ...pageSegmentView, ...MINIMAL_VIEWPORTS },
+    defaultViewport: "pageNarrow",
+  },
   backgrounds: {
+    default: "light",
     grid: {
       cellSize: 14,
-      opacity: 0.5,
-      cellAmount: 4,
-      offsetX: 16,
-      offsetY: 16,
+      opacity: 0.2,
+      cellAmount: 2,
     },
+  },
+  darkMode: {
+    // Override the default dark theme
+    dark: { ...themes.dark },
+    // Override the default light theme
+    light: { ...themes.normal },
+    current: "dark",
   },
 };
 
