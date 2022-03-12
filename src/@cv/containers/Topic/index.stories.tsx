f953b6c2394e8a56a151f100bb8b26cc5a971f57
Dino Scheidt <github@dino-scheidt.de>
2022-03-12 03:41:44 +0100

    ✅ Added basic stories to all containers

---
+++ src/@cv/containers/Topic/index.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import Component from "./index";
+
+export default {
+  component: Component,
+} as ComponentMeta<typeof Component>;
+
+const Template: ComponentStory<typeof Component> = (args) => (
+  <Component {...args} />
+);
+
+export const Index = Template.bind({});
+Index.args = { title: "Example Inc." };
