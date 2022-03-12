a4636fba4ba40b54af04514f7f7bbe19cf39fa25
Dino Scheidt <github@dino-scheidt.de>
2022-03-12 21:38:22 +0100

    🔨🚧 Separating Component Domains

---
+++ src/@cv/containers/Layout/index.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import FooterSection from "./Footer";
+import Header from "./Header";
+import MainSection from "./Main";
+import MetaSection from "./Meta";
+
+import { x } from "@xstyled/emotion";
+
+import {
+  goldenRatioContentSpacer,
+  goldenRatioElementSpacer,
+  goldenRatioPageHeight,
+  goldenRatioPageMargin,
+  goldenRatioPageWidth,
+  pxH,
+  pxW,
+} from "@cv/views/DesignSystem";
+
+interface IPage {}
+
+const Page: React.FC<{}> = ({ children, ...props }) => {
+  return (
+    <>
+      <x.div
+        bg="white"
+        color="black"
+        /* Page Dimensions (A4) */
+        w={pxW(goldenRatioPageWidth)}
+        h={pxH(goldenRatioPageHeight)}
+        /* Trim (A4) */
+        px={pxW(goldenRatioPageMargin)}
+        pt={pxH(goldenRatioPageMargin)}
+        pb={pxH(goldenRatioContentSpacer)}
+        /* Box Model */
+        display="flex"
+        flexDirection="column"
+        spaceY={pxH(goldenRatioElementSpacer)}
+      >
+        {/* START: CV Sections */}
+
+        <Header />
+        <MetaSection />
+        <MainSection />
+
+        {/* Footer */}
+        <FooterSection />
+        {/* END: Footer */}
+        {/* END: CV Sections */}
+      </x.div>
+    </>
+  );
+};
+
+export default Page;
--- src/@cv/views/index.tsx
+++ src/@cv/views/index.tsx
@@ -2,19 +2,9 @@ import React from "react";
 
 import styled, { x } from "@xstyled/emotion";
 
-import DesignSystem, {
-  goldenRatioContentSpacer,
-  goldenRatioElementSpacer,
-  goldenRatioPageHeight,
-  goldenRatioPageMargin,
-  goldenRatioPageWidth,
-  pxH,
-  pxW,
-} from "@cv/views/DesignSystem";
-import FooterSection from "@cv/views/Page/Footer";
-import Header from "@cv/views/Page/Header";
-import MainSection from "@cv/views/Page/Main";
-import MetaSection from "@cv/views/Page/Meta";
+import DesignSystem from "@cv/views/DesignSystem";
+
+import Page from "@cv/containers/Layout";
 
 /**
  * Animation for responsiveness.
@@ -40,32 +30,7 @@ const CV: React.FC<ICVProps> = (props) => {
           /* TODO: Discriminate between iphone mini and iphone max style phones. */
           scale={{ _: "0.45", sm: "0.75", md: "0.9", lg: "1", xl: "1.4" }}
         >
-          <x.div
-            bg="white"
-            color="black"
-            /* Page Dimensions (A4) */
-            w={pxW(goldenRatioPageWidth)}
-            h={pxH(goldenRatioPageHeight)}
-            /* Trim (A4) */
-            px={pxW(goldenRatioPageMargin)}
-            pt={pxH(goldenRatioPageMargin)}
-            pb={pxH(goldenRatioContentSpacer)}
-            /* Box Model */
-            display="flex"
-            flexDirection="column"
-            spaceY={pxH(goldenRatioElementSpacer)}
-          >
-            {/* START: CV Sections */}
-
-            <Header />
-            <MetaSection />
-            <MainSection />
-
-            {/* Footer */}
-            <FooterSection />
-            {/* END: Footer */}
-            {/* END: CV Sections */}
-          </x.div>
+          <Page />
         </PageScale>
       </x.div>
     </DesignSystem>
