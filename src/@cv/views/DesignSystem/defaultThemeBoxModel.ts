a9abc5ca02f3a9c4ef202c46f42c95de8bdaf58d
Dino Scheidt <github@dino-scheidt.de>
2022-04-22 04:11:30 +0200

    🔥 Removing theme property transform

--- src/@cv/views/DesignSystem/defaultThemeBoxModel.ts
+++ src/@cv/views/DesignSystem/defaultThemeBoxModel.ts
@@ -13,44 +13,6 @@ const boxModel = {
 };
 
 /**
- * Selection of box model relevant theme preset objects eligible for auto processing.
- */
-type legalBoxModelSubGroups =
-  | typeof defaultTheme["space"]
-  | typeof defaultTheme["sizes"]
-  | typeof defaultTheme["radii"]
-  | typeof defaultTheme["fontSizes"]
-  | typeof defaultTheme["lineHeights"];
-
-/**
- * Ability to replace parts of strings within xStyles default theme presets.
- * Defaults to rem -> em. Only assignable to box model related presets.
- */
-const replaceUnitInBoxModel = (
-  boxModelGroup: legalBoxModelSubGroups,
-  findUnit = "rem",
-  replaceWith = "em",
-): legalBoxModelSubGroups => {
-  // Iterate over presets
-  Object.entries(boxModelGroup).forEach(([preset, style]) => {
-    // TODO: Typescript - This could be cleaner but no time to type upstream.
-
-    if (typeof style === "string" || style instanceof String) {
-      // Explanation: This is the infamous "not assignable to never" guard for "improperly" instantiated arrays.
-
-      // https://stackoverflow.com/questions/68075836/how-to-fix-type-string-is-not-assignable-to-type-never#comment120321098_68075836
-      // @ts-expect-error TS2322: Type 'string' is not assignable to type 'never'.
-      boxModelGroup[preset as keyof typeof boxModelGroup] = style.replace(
-        findUnit,
-        replaceWith,
-      );
-    }
-  });
-
-  return boxModelGroup;
-};
-
-/**
  * Applying Box Model to default xStyled Theme Object.
  *
  * Since xStyled uses "rem" by default, this also insures that the
@@ -61,23 +23,23 @@ const replaceUnitInBoxModel = (
 const defaultThemeBoxModel = {
   ...defaultTheme,
   space: {
-    ...replaceUnitInBoxModel(defaultTheme.space),
+    ...defaultTheme.space,
     ...boxModel.space,
   },
   sizes: {
-    ...replaceUnitInBoxModel(defaultTheme.sizes),
+    ...defaultTheme.sizes,
     ...boxModel.sizes,
   },
   radii: {
-    ...replaceUnitInBoxModel(defaultTheme.radii),
+    ...defaultTheme.radii,
     ...boxModel.radii,
   },
   fontSizes: {
-    ...replaceUnitInBoxModel(defaultTheme.fontSizes),
+    ...defaultTheme.fontSizes,
     ...boxModel.fontSizes,
   },
   lineHeights: {
-    ...replaceUnitInBoxModel(defaultTheme.lineHeights),
+    ...defaultTheme.lineHeights,
     ...boxModel.lineHeights,
   },
 };
